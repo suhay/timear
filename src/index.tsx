98da747326b17dd0011284e70e5027bec3d449a3
Matt Suhay <matthew.suhay@gmail.com>
2023-01-10 19:56:32 -0500

    Navermind

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import ReactDom from 'react-dom'
 
 import { App } from './component/app'
-import { TimearYamlType } from './types/yaml/timear'
 
 const rootNode = document.getElementById('timear')
 
 ReactDom.render(<App />, rootNode)
-
-// Allow to be inserted into ExothermicJS application
-export const register = {
-  tags: {
-    timear: TimearYamlType,
-  },
-}
